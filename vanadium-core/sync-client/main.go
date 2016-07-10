e1f36a47fa753bac845c6df896b0b1cbc3a35ccc
Razvan Musaloiu-E <razvanm@google.com>
2016-07-10 14:48:11 -0700

    Add support for streaming to the Vanadium Core client

--- vanadium-core/sync-client/main.go
+++ vanadium-core/sync-client/main.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"math/rand"
+	"os"
 	"time"
 
 	"github.com/razvanm/rpc-benchmarks/vanadium-core"
-	"math/rand"
 	"v.io/v23"
-	_ "v.io/x/ref/runtime/factories/generic"
 	"v.io/v23/context"
-	"fmt"
+	_ "v.io/x/ref/runtime/factories/generic"
 	"v.io/x/ref/test/benchmark"
-	"os"
 )
 
 var (
-	server   = flag.String("server", "", "Name of the server to connect to")
-	warmup   = flag.Duration("warmup", time.Second, "Duration of the warmup")
 	duration = flag.Duration("duration", 10*time.Second, "Duration of the benchmark")
+	server   = flag.String("server", "", "Name of the server to connect to")
 	size     = flag.Uint("size", 0, "Size of the payload")
+	stream   = flag.Bool("stream", false, "Use streaming RPCs")
+	warmup   = flag.Duration("warmup", time.Second, "Duration of the warmup")
 
-	stub sync.SyncClientStub
 	rootCtx *context.T
+	stub    sync.SyncClientStub
 )
 
 func loop(duration time.Duration, payload []byte) *benchmark.Stats {
@@ -44,6 +45,30 @@ func loop(duration time.Duration, payload []byte) *benchmark.Stats {
 	}
 }
 
+func loopStream(duration time.Duration, payload []byte) *benchmark.Stats {
+	stats := benchmark.NewStats(16)
+	call, err := stub.SyncStream(rootCtx)
+	if err != nil {
+		panic(err)
+	}
+	stream := call.SendStream()
+	end := time.After(duration)
+	for {
+		select {
+		case <-end:
+			return stats
+		default:
+			start := time.Now()
+			err = stream.Send(payload)
+			elapsed := time.Since(start)
+			if err != nil {
+				panic(err)
+			}
+			stats.Add(elapsed)
+		}
+	}
+}
+
 func main() {
 	ctx, shutdown := v23.Init()
 	defer shutdown()
@@ -56,8 +81,15 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Warming up for %s...\n", *warmup)
-	loop(*warmup, payload)
-	fmt.Printf("Running the benchmark for %s...\n", *duration)
-	loop(*duration, payload).Print(os.Stdout)
+	if *stream {
+		fmt.Printf("Warming up for %s...\n", *warmup)
+		loopStream(*warmup, payload)
+		fmt.Printf("Running the benchmark for %s...\n", *duration)
+		loopStream(*duration, payload).Print(os.Stdout)
+	} else {
+		fmt.Printf("Warming up for %s...\n", *warmup)
+		loop(*warmup, payload)
+		fmt.Printf("Running the benchmark for %s...\n", *duration)
+		loop(*duration, payload).Print(os.Stdout)
+	}
 }
