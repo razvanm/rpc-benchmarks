fa6d2ca61dc97a339240ce67b56f001bad01018e
Razvan Musaloiu-E <razvanm@google.com>
2016-07-11 17:16:03 -0700

    Enable pprof for the gRPC server

--- grpc/sink-server-grpc/main.go
+++ grpc/sink-server-grpc/main.go
@@ -4,20 +4,30 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	_ "net/http/pprof"
 
 	"github.com/razvanm/rpc-benchmarks/grpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"net/http"
 )
 
 var (
 	address  = flag.String("address", ":0", "Local address to listen on")
 	certFile = flag.String("cert", "certs/server.pem", "TLS cert file")
 	keyFile  = flag.String("key", "certs/server.key", "TLS key file")
+	debugAddress = flag.String("debugAddress", ":0", "Local address to listen on")
 )
 
 func main() {
 	flag.Parse()
+
+	go func() {
+		fmt.Printf("Debug listening on %v\n", *debugAddress)
+		if err := http.ListenAndServe(*debugAddress, nil); err != nil {
+			panic(err)
+		}
+	}()
 
 	listener, err := net.Listen("tcp", *address)
 	if err != nil {
