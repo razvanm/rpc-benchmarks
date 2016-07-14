26607da9880df9c31713c72619895216cb828d96
Razvan Musaloiu-E <razvanm@google.com>
2016-07-14 12:32:26 -0700

    Add some benchmark results comparing Vanadium and gRPC
    The tests use a single-threaded client sending non-streaming/streaming
    RPCs to a sink server. Each configuration runs for a minute. The
    client and server are on different machines, both an Intel Haswell
    n1-standard-1 (1 vCPU and 3.75 GB RAM) in the us-east1-d zone.
    
    Median QPS for various payload sizes:
    
             Vanadium           gRPC           Vanadium               gRPC
             --------- non-streaming --------  -------- streaming --------
       0B    1725.31          3760.23 (+117%)   73319.15 (+15%)   63588.96
       1KiB  1660.51          3255.31 (+96%)    54773.51 (+50%)   36357.02
      10KiB  1395.35          1995.64 (+43%)    20470.82 (+138%)   8570.08
     100KiB   746.22 (+50%)    495.47            2766.05 (+306%)    680.79
       1MiB   181.17 (+194%)    61.57             237.43 (+276%)     63.08
       2MiB    93.73 (+206%)    30.59             108.84 (+249%)     31.11
       5MiB    32.34 (+172%)    11.86              41.11 (+235%)     12.24
      10MiB    17.62 (+193%)     6.01              19.25 (+213%)      6.14
      25MiB     7.26 (+198%)     2.43               7.44 (+205%)      2.43

---
+++ results/plot-nostream.R
@@ -0,0 +1,90 @@
+all <- read.table('run5.clean', col.names=c('pos', 'total', 'val', 'type', 'size', 'stype'))
+all$size <- factor(all$size)
+all$val <- all$val/1000000
+all$qps <- 1000/all$val
+
+red <- c('#500003','#6C0D10','#87201C','#A0342A','#B74B3B','#CB624E','#DD7C64','#EA977C','#F3B397','#F7D1B4')
+green <- c('#022609','#0E3C16','#205225','#356A36','#4D8148','#68995D','#85B174','#A6C98C','#C9E0A7','#EFF8C4')
+sizeName <- function(val) {
+  val <- as.numeric(val)
+  if (val == 0) {
+    return('0')
+  }
+  if (val < 1024*1024) {
+    return(sprintf('%dK', val / 1024))
+  }
+  sprintf('%dM', val / 1024 / 1024)
+}
+
+plot.percent <- function(y, x1, x2, col) {
+  rect(y-0.25, x1, y+0.25, x2, col=col, border=col)
+}
+
+plot.one <- function(typeArg, sizeArg, stypeArg, y, pallete) {
+  d <- subset(all, type == typeArg & size == sizeArg & stype == stypeArg)
+  d <- d$qps
+
+  plot.percent(y, d[1], d[100], pallete[7])  # 98% of the data
+  plot.percent(y, d[5], d[95],  pallete[5])  # 90% of the data
+  plot.percent(y, d[25], d[75], pallete[1])  # 50% of the data
+  segments(y-0.3, d[50], y+0.3, d[50], lwd=3, lend=1, col='white')
+  print(c(d[1], d[50], sizeArg))
+  mtext(sizeName(sizeArg), side=1, at=y, las=1, line=1)
+#  mtext(typeArg, side=1, at=y, las=1, line=2, cex=0.8)
+  mtext(format(d[50], digits=2), side=1, at=y, las=1, line=3.5, cex=0.8)
+  return(d[50])
+}
+
+png('plot-nostream.png', width=1200, height=800, bg='white')
+par(family='DejaVu Sans')
+par(mar=c(6, 4, 5, 0), fig=c(0, 0.5, 0, 1))
+
+xrange <- c(0, 5000)
+plot(c(0, 9), xrange, type='n', ann=F, axes=F, xaxs='i', yaxs='i')
+
+i <- 1
+for (size in levels(all$size)[1:4]) {
+  m1 <- plot.one('v23', size, 'nostream', i, green)
+  m2 <- plot.one('grpc', size, 'nostream', i+1, red)
+  if (m1 > m2) {
+    mtext(sprintf("+%.1f%%", m1/m2*100-100), side=1, at=i, las=1, line=4.5, cex=0.8, col='gray')
+  } else {
+    mtext(sprintf("+%.1f%%", m2/m1*100-100), side=1, at=i+1, las=1, line=4.5, cex=0.8, col='gray')
+  }
+  i <- i + 2
+}
+
+x <- seq(xrange[1], xrange[2], 100)
+axis(2, x, labels=F, las=2, lwd=0, lwd.tick=0.5, tcl=-0.3)
+x <- seq(xrange[1], xrange[2], 1000)
+axis(2, x, labels=x, lwd=0, las=1, lwd.tick=2)
+mtext('Payload size \u2192', side=1, at=-0.15, las=1, line=1)
+mtext('Median QPS', side=1, at=-0.25, las=1, line=3.5, cex=0.8)
+
+par(mar=c(6, 4, 5, 2), fig=c(0.5, 1, 0, 1), new=T)
+xrange <- c(0, 230)
+plot(c(0, 11), xrange, type='n', ann=F, axes=F, xaxs='i', yaxs='i')
+
+i <- 1
+for (size in levels(all$size)[5:9]) {
+  m1 <- plot.one('v23', size, 'nostream', i, green)
+  m2 <- plot.one('grpc', size, 'nostream', i+1, red)
+  if (m1 > m2) {
+    mtext(sprintf("+%.1f%%", m1/m2*100-100), side=1, at=i, las=1, line=4.5, cex=0.8, col='gray')
+  } else {
+    mtext(sprintf("+%.1f%%", m2/m1*100-100), side=1, at=i+1, las=1, line=4.5, cex=0.8, col='gray')
+  }
+  i <- i + 2
+}
+
+x <- seq(xrange[1], xrange[2], 1)
+axis(2, x, labels=F, las=2, lwd=0, lwd.tick=0.5, tcl=-0.3)
+x <- seq(xrange[1], xrange[2], 10)
+axis(2, x, labels=format(x, nsmall=1), lwd=0, las=1, lwd.tick=2)
+
+legend('topright', inset=0.02, bty='n', pch=15,
+       c('Vanadium', 'gRPC'), col=c(green[5], red[5]), pt.cex=2, cex=1.4)
+
+par(mar=c(6, 0, 3, 0), fig=c(0, 1, 0, 1), new=T)
+title(main='Vanadium vs gRPC QPS for non-streaming RPCs', cex.main=2)
+dev.off()
